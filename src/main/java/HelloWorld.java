695e3aea28f414ccf9d4ab7d3e0db0b28ec1b838
millsmcilroy <millsmcilroy@gmail.com>
2016-05-11 16:35:11 -0600

    BUILD SUCCESSFUL

--- src/main/java/HelloWorld.java
+++ src/main/java/HelloWorld.java
@@ -1,2 +1,14 @@
 public class HelloWorld {
+  public static String hello (String name) {
+    if (name == null) {
+      return "Hello, World!";
+    }
+    else if (name.isEmpty()) {
+      return "Hello, World!";
+    }
+    else {
+      return "Hello, " + name + "!";
+    }
+
+  }
 }
